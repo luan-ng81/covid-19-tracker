c9f9aa842bc1f7c8ee56f5b01944434247d68c35
luan-ng81 <30273108+luan-ng81@users.noreply.github.com>
2021-02-08 16:43:52 -0800

    Modified multiplier

---
+++ build/precache-manifest.4f8c9b1a48bf719953ba05bb71076729.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf783104168e0cb856ba018b5beb39a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "e6528a629799b1210181",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "5d7d375ef0d7e8ca00c8",
+    "url": "/static/js/2.c566785b.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.c566785b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6528a629799b1210181",
+    "url": "/static/js/main.6d9cb1bd.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
